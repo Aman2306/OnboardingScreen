96a92c0ace91829c87a53e23b19b6456f8dad3cd
Aman Meena <amanmeena@yahoo.com>
2019-02-21 20:59:10 +0530

    Initial Commit

---
+++ DemoScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoScreen
+//
+//  Created by Aman Meena on 21/02/19.
+//  Copyright © 2019 Aman Meena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
